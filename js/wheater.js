63705b71a45e4f043b7d6a5a09fd1cb013d5dbb0
Andrew Herculano <andrewherculano@hotmail.com>
2021-07-03 11:47:41 -0300

    request at city weather data

--- js/wheater.js
+++ js/wheater.js
@@ -21,5 +21,23 @@ const getCityData = async (cityName) => {
   }
 }
 
-getCityData('São Paulo')
+const getCityWeatherData = async (cityName) => {
+  try {
+    const { Key } = await getCityData(cityName)
+    const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br`
+    const response = await fetch(cityWeatherUrl)
+    
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados')
+    }
+    
+    const cityWeatherData = await response.json()
+    console.log(cityWeatherData)
+    
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`)
+  }
+}
+
+getCityWeatherData('Tokyo')
 
