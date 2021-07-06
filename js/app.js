04a099568648bc9ba1c34603fe1cd173b2e1cb7a
Andrew Herculano <andrewherculano@hotmail.com>
2021-07-06 19:23:34 -0300

    show infos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 const cityForm = document.querySelector('[data-js="search-city"]')
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
+const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]')
+const cardInfos = document.querySelector('[data-js="show-card"]')
 
 cityForm.addEventListener('submit', async (event) => {
   event.preventDefault()
@@ -7,6 +11,13 @@ cityForm.addEventListener('submit', async (event) => {
   const [{ Key, LocalizedName }] = await getCityData(inputValue)
   const [{ WeatherText, Temperature }] = await getCityWeather(Key)
 
-  console.log(WeatherText, Temperature.Metric.Value)
+  if (cardInfos.classList.contains('display-none')) {
+    cardInfos.classList.remove('display-none')
+  }
+
+  cityNameContainer.textContent = LocalizedName
+  cityWeatherContainer.textContent = WeatherText
+  cityTemperatureContainer.textContent = Temperature.Metric.Value
+
   cityForm.reset()
 })
