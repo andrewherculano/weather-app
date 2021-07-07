b56f57159567fca5da9ebe87f0c3fbbdbf54c52d
Andrew Herculano <andrewherculano@hotmail.com>
2021-07-07 19:09:52 -0300

    refactor app.js

--- js/app.js
+++ js/app.js
@@ -7,28 +7,40 @@ const weatherIconContainer = document.querySelector('[data-js="show-weather-icon
 
 let imageCard = document.querySelector('[data-js="img-card"]')
 
-cityForm.addEventListener('submit', async (event) => {
-  event.preventDefault()
-
-  const inputValue = event.target.city.value
-  const [{ Key, LocalizedName }] = await getCityData(inputValue)
-  const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
-  const weatherIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
-
+const showImageCard = () => {
   if (cardInfos.classList.contains('display-none')) {
     cardInfos.classList.remove('display-none')
   }
+}
 
-  if (IsDayTime) {
-    imageCard.src = './src/day.svg'
-  } else {
-    imageCard.src = './src/night.svg'
-  }
+const showInfosCityWeather = async (cityName) => {
+  const [{
+    Key,
+    LocalizedName
+  }] = await getCityData(cityName)
+
+  const [{
+    WeatherText,
+    Temperature,
+    IsDayTime,
+    WeatherIcon
+  }] = await getCityWeather(Key)
+  
+  const weatherIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
+
+  IsDayTime ? imageCard.src = './src/day.svg' : imageCard.src = './src/night.svg'
 
   weatherIconContainer.innerHTML = weatherIcon
   cityNameContainer.textContent = LocalizedName
   cityWeatherContainer.textContent = WeatherText
   cityTemperatureContainer.textContent = Temperature.Metric.Value
+}
 
+cityForm.addEventListener('submit', event => {
+  event.preventDefault()
+  const inputValue = event.target.city.value
+
+  showInfosCityWeather(inputValue)
+  showImageCard()
   cityForm.reset()
 })
